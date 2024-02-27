7d06c808a276777632a15a5e5f1c05ababd769ae
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-02-28 03:24:15 +0400

    Add method into service

--- AvToDoList/Services/ToDoListService.cs
+++ AvToDoList/Services/ToDoListService.cs
@@ -1,6 +1,14 @@
-﻿namespace AvToDoList.Services;
+﻿using System.Collections.Generic;
+using AvToDoList.DataModel;
+
+namespace AvToDoList.Services;
 
 public class ToDoListService
 {
-	
+	public IEnumerable<ToDoItem> GetItems() => new[]
+	{
+		new ToDoItem() { Description = "Walk the milk" },
+		new ToDoItem() { Description = "Buy some cat" },
+		new ToDoItem() { Description = "Learn Avalonia", IsChecked = true }
+	};
 }
