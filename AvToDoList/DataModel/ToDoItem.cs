a00c4cbda9b075343877c56624f5be84e7d063ef
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-02-28 03:18:18 +0400

    Add properties into ToDoItem model class

--- AvToDoList/DataModel/ToDoItem.cs
+++ AvToDoList/DataModel/ToDoItem.cs
@@ -6,4 +6,16 @@ public class ToDoItem
 {
 	string _description = String.Empty;
 	bool _isChecked;
+	
+	public string Description
+	{
+		get { return _description; }
+		set { _description = value; }
+	}
+
+	public bool IsChecked
+	{
+		get { return _isChecked; }
+		set { _isChecked = value; }
+	}
 }
