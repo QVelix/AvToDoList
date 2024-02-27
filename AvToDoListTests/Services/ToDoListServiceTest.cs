4985048c22b48081484524bb8f3a06f17e8123ba
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-02-28 03:40:01 +0400

    Write tests for ToDoListService.GetItems method

---
+++ AvToDoListTests/Services/ToDoListServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using AvToDoList.DataModel;
+using AvToDoList.Services;
+using JetBrains.Annotations;
+
+namespace AvToDoListTests.Services;
+
+[TestSubject(typeof(ToDoListService))]
+public class ToDoListServiceTest
+{
+	[Fact]
+	public void GetItems_returns_ToDoItemEnumerable()
+	{
+		ToDoListService test = new ToDoListService();
+		var result = test.GetItems();
+		Assert.IsAssignableFrom<IEnumerable<ToDoItem>>(result);
+	}
+
+	[Fact]
+	public void GetItems_returns_three_items()
+	{
+		ToDoListService test = new ToDoListService();
+		var result = test.GetItems();
+		Assert.Equal(3, result.Count());
+	}
+}
