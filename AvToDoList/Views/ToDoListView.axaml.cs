8451f1fee5f52c18bbdc132131b112655f57cb22
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-02-28 02:33:55 +0400

    Add view user control

---
+++ AvToDoList/Views/ToDoListView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvToDoList.Views;
+
+public partial class ToDoListView : UserControl
+{
+	public ToDoListView()
+	{
+		InitializeComponent();
+	}
+}
