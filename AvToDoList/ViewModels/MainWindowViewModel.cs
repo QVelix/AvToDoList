ab36cd287a188fde8f8f06574c8be2ae3d5114ca
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-02-29 05:48:51 +0400

    Add ViewModelBase field and property and AddItem method that calls ViewModel AddItemViewModel

--- AvToDoList/ViewModels/MainWindowViewModel.cs
+++ AvToDoList/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using AvToDoList.Services;
+using ReactiveUI;
 
 namespace AvToDoList.ViewModels;
 
@@ -8,15 +9,28 @@ public class MainWindowViewModel : ViewModelBase
 	public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
 	private ToDoListViewModel _toDoList;
+	private ViewModelBase _contentViewModel;
 
 	public MainWindowViewModel()
 	{
 		var service = new ToDoListService();
 		_toDoList = new ToDoListViewModel(service.GetItems());
+		_contentViewModel = ToDoList;
 	}
 
 	public ToDoListViewModel ToDoList
 	{
 		get { return _toDoList; }
+	}
+
+	public ViewModelBase ContentViewModel
+	{
+		get { return _contentViewModel; }
+		private set { this.RaiseAndSetIfChanged(ref _contentViewModel, value); }
+	}
+
+	public void AddItem()
+	{
+		ContentViewModel = new AddItemViewModel();
 	}
 }
