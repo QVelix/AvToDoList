922862b87e5c7b69b1605aa921a6987a15ffce01
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-02-29 05:04:23 +0400

    Write a constructor dependment on the "ToDoListService" and "ToDoItem" model

--- AvToDoList/ViewModels/MainWindowViewModel.cs
+++ AvToDoList/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,22 @@
-﻿namespace AvToDoList.ViewModels;
+﻿using AvToDoList.Services;
+
+namespace AvToDoList.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
 	public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
+	private ToDoListViewModel _toDoList;
+
+	public MainWindowViewModel()
+	{
+		var service = new ToDoListService();
+		_toDoList = new ToDoListViewModel(service.GetItems());
+	}
+
+	public ToDoListViewModel ToDoList
+	{
+		get { return _toDoList; }
+	}
 }
