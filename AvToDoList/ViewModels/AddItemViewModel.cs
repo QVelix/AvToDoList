ceea9b44647eec0d1c51e6e8ec8782c570576193
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-03-03 12:57:08 +0400

    Add more fields

--- AvToDoList/ViewModels/AddItemViewModel.cs
+++ AvToDoList/ViewModels/AddItemViewModel.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.Reactive;
+using AvToDoList.DataModel;
+using ReactiveUI;
 
 namespace AvToDoList.ViewModels;
 
 public class AddItemViewModel : ViewModelBase
 {
 	private string _description = String.Empty;
-
+	
 	public string Description
 	{
 		get { return _description; }
-		set { _description = value; }
+		set { this.RaiseAndSetIfChanged(ref _description, value); }
 	}
+	public ReactiveCommand<Unit, ToDoItem> OkCommand { get; }
+	public ReactiveCommand<Unit, Unit> CancelCommand { get; }
+	
 }
